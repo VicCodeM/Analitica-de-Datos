641eedb10ca84ceb4f73321bd01d5df1734f18c1
Victor Maldonado <vicomatters@live.com>
2024-10-25 02:37:29 -0600

    .

--- Unidad 2/Redes Nueronales/parcticaregresion.py
+++ Unidad 2/Redes Nueronales/parcticaregresion.py
@@ -3,7 +3,7 @@ from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn import metrics
-import pandas as pd
+import matplotlib.pyplot as plt
 
 # Cargar el dataset
 cal_housing = fetch_california_housing()
@@ -12,7 +12,7 @@ X, y = cal_housing.data, cal_housing.target
 # Dividir los datos en conjuntos de entrenamiento y prueba
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
-# Estandarizar los datos eliminando la media y escalando para que la varianza sea igual a 1
+# Estandarizar los datos
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
@@ -35,4 +35,15 @@ print(f"R² Score: {r2:.2f}")
 
 # Mostrar las primeras predicciones y los valores reales
 print("Predicciones:", y_pred[:5]) 
-print("Valores reales:", y_test[:5])
+print("Valores reales:", y_test[:5]) 
+
+
+# Gráfico 1: Diagrama de dispersión de Predicciones vs Valores Reales
+plt.figure(figsize=(8, 6))
+plt.scatter(y_test, y_pred, alpha=0.5, color="blue")
+plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--', lw=2)
+plt.xlabel('Valores Reales')
+plt.ylabel('Predicciones')
+plt.title('Predicciones vs Valores Reales')
+plt.show()
+
